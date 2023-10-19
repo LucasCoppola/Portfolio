00de05806a1eecafe98115b8b533d92f7a6998b1
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-19 20:04:19 -0300

    init

--- components/about.tsx
+++ components/about.tsx
@@ -1,49 +1,40 @@
-"use client";
+'use client'
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
+import React from 'react'
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+	const { ref } = useSectionInView('About')
 
-  return (
-    <motion.section
-      ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-      id="about"
-    >
-      <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
+	return (
+		<motion.section
+			ref={ref}
+			className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+			initial={{ opacity: 0, y: 100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.175 }}
+			id="about"
+		>
+			<SectionHeading>About me</SectionHeading>
+			<p className="mb-3">
+				My name is <span className="font-semibold">Lucas Coppola</span> and i'm from{' '}
+				<span className="italic">Buenos Aires, Argentina.</span>
+				<br />
+				When i got into coding i didn't have a big 'aha' moment, it was more of a gradual evolution. I tried it
+				out, and I kept going, one line of code at a time. My curiosity and the confidence that I could learn
+				and tackle more complex projects kept me pushing forward.
+			</p>
 
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
-    </motion.section>
-  );
+			<p>
+				My playground of choice is the <span className="font-semibold">T3 Stack</span>, where I dive deep into{' '}
+				<span className="italic">React, Next.js, TypeScript, Tailwind, and Prisma.</span> I'm also familiar and
+				enjoy working with the <span className="font-semibold">MERN</span> stack.
+				<br />
+				<br />
+				By the way, I'm always up for diving into new tech stuff.
+			</p>
+		</motion.section>
+	)
 }
