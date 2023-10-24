ca268a0a355a4cd6d4062fe4a0a974a2425d6fd9
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-24 13:26:18 -0300

    update form contact to cards contact

---
+++ components/contact-card.tsx
@@ -0,0 +1,47 @@
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion'
+import Link from 'next/link'
+import { MouseEvent } from 'react'
+
+export default function ContactCard({
+	link
+}: {
+	link: { name: string; username: string; icon: JSX.Element; link?: string }
+}) {
+	let mouseX = useMotionValue(0)
+	let mouseY = useMotionValue(0)
+
+	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+		let { left, top } = currentTarget.getBoundingClientRect()
+
+		mouseX.set(clientX - left)
+		mouseY.set(clientY - top)
+	}
+
+	return (
+		<Link href={link.link || ''} target="_blank">
+			<div
+				className="group relative w-72 rounded-xl border border-white/10 bg-gray-900 px-8 pt-6 pb-12 shadow-2xl"
+				onMouseMove={handleMouseMove}
+			>
+				<motion.div
+					className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+					style={{
+						background: useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              rgba(14, 165, 233, 0.2),
+              transparent 50%
+            )
+          `
+					}}
+				/>
+				<div className="flex flex-col items-center justify-center relative">
+					<i>{link.icon}</i>
+
+					<p className="mt-6 text-xl font-bold tracking-tighter text-gray-300">{link.username}</p>
+					<span className="mt-2 text-sm text-zinc-400">{link.name}</span>
+				</div>
+			</div>
+		</Link>
+	)
+}
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,74 +1,59 @@
-"use client";
+'use client'
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
-import SubmitBtn from "./submit-btn";
-import toast from "react-hot-toast";
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
+import { useSectionInView } from '@/lib/hooks'
+import ContactCard from './contact-card'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { BiLogoGmail } from 'react-icons/bi'
+
+const links = [
+	{
+		name: 'Github',
+		link: 'https://github.com/LucasCoppola',
+		username: 'Lucas Coppola',
+		icon: <AiFillGithub className="h-12 w-12 z-10" />
+	},
+	{
+		name: 'LinkedIn',
+		link: 'https://www.linkedin.com/in/lucas-coppola',
+		username: 'Lucas Coppola',
+		icon: <AiFillLinkedin className="h-12 w-12 z-10" />
+	},
+	{
+		name: 'Gmail',
+		username: 'lucascoppola21@gmail.com',
+		icon: <BiLogoGmail className="h-12 w-12 z-10" />
+	}
+]
 
 export default function Contact() {
-  const { ref } = useSectionInView("Contact");
+	const { ref } = useSectionInView('Contact')
 
-  return (
-    <motion.section
-      id="contact"
-      ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-    >
-      <SectionHeading>Contact me</SectionHeading>
-
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
-        </a>{" "}
-        or through this form.
-      </p>
-
-      <form
-        className="mt-10 flex flex-col dark:text-black"
-        action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
-
-          if (error) {
-            toast.error(error);
-            return;
-          }
-
-          toast.success("Email sent successfully!");
-        }}
-      >
-        <input
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="senderEmail"
-          type="email"
-          required
-          maxLength={500}
-          placeholder="Your email"
-        />
-        <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="message"
-          placeholder="Your message"
-          required
-          maxLength={5000}
-        />
-        <SubmitBtn />
-      </form>
-    </motion.section>
-  );
+	return (
+		<motion.section
+			id="contact"
+			ref={ref}
+			className="mb-20 sm:mb-28 w-[min(100%,60rem)] text-center"
+			initial={{
+				opacity: 0
+			}}
+			whileInView={{
+				opacity: 1
+			}}
+			transition={{
+				duration: 1
+			}}
+			viewport={{
+				once: true
+			}}
+		>
+			<SectionHeading>Contact me</SectionHeading>
+			<div className="flex justify-center gap-9 mt-16">
+				{links.map((link, i) => (
+					<ContactCard key={i} link={link} />
+				))}
+			</div>
+		</motion.section>
+	)
 }
